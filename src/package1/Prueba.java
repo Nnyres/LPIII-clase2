bded480696d417d6f367f1ccb948f84804299913
Andres <johnnymurillo47@gmail.com>
2023-08-12 11:10:44 -0500

    Creación de main

---
+++ src/package1/Prueba.java
@@ -0,0 +1,12 @@
+package src.package1;
+
+public class Prueba {
+
+    public Prueba(String cuañquiera){
+    }
+
+    void test(){
+        System.out.printf("Cualquier string");
+    }
+
+}
---
+++ src/package2/main.java
@@ -0,0 +1,14 @@
+package src.package2;
+
+import src.package1.Prueba;
+
+import javax.sound.midi.Soundbank;
+
+public class main {
+
+    Prueba prueba = new Prueba();
+    System.out.printf("Aqui Arranque");
+
+
+
+}
