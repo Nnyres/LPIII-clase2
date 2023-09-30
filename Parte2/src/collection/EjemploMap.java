e3a637e7ec5d7b2348e3cddda780e60566d99d38
Nnyres <johnnymurillo47@gmail.com>
2023-09-30 11:12:06 -0500

    Colecciones 2

--- Parte2/src/collection/EjemploMap.java
+++ Parte2/src/collection/EjemploMap.java
@@ -1,5 +1,8 @@
 package collection;
 
+import com.sun.jdi.Value;
+
+import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +11,22 @@ public class EjemploMap {
         Map<Integer, String> map = new HashMap<>();
         map.put(4, "AccionEliminar");
         map.put(3, "AccionMostrar");
+       // System.out.println(map);
+
+        map.put(20, "Accioncualquiera");
+        map.put(11, "Accioncualquiera");
+        map.put(11, "Accioncualquiera5");//Cuando el Integer es igual se sobreescrine la accion
+        //System.out.println(map);
+        String value20 = map.get(20);
+       // System.out.println(value20);
+
+        map.forEach((key, value) -> System.out.println("Key" + key + "->" + "Value" + value));
         System.out.println(map);
+
+
+
+
+
+
     }
 }
